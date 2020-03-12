2bd997a38d78616acb36be5d5c526c85e0414352
Penelope L Owens <owenspennie9@live.com>
2020-03-11 22:21:35 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, {
+  Component
+} from 'react';
+import Images from './Images';
+import SearchBar from './SearchBar';
+
+
+class App extends Component {
+  return ( <
+    main className = 'App' > {
+      /* content goes here */
+    } <
+    /main>
+  );
+}
+
+export default App;
